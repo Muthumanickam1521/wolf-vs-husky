638a16f816402b7b9a48ed6c5d7a962976061681
Muthumanickam V <pearlrubymv@gmail.com>
2023-08-04 23:31:18 +0530

    classifier

---
+++ image_classifier.py
@@ -0,0 +1,30 @@
+import keras
+import numpy as np
+from tensorflow.keras import models, layers, Sequential
+
+def wolf_or_husky_classifier(image, model_weights_path):
+    '''	
+    A simple image classifier function that takes 
+    image path and model weights path as inputs
+    '''
+    weights = np.load(model_weights_path, allow_pickle = True) # loading pre_trained model weights
+    
+    resize_and_normalize = Sequential([ # model with two pre-processing layers
+    	layers.Resizing(256, 256), # layer1: resize
+    	layers.Rescaling(1/255) # layer2: rescale
+    ])
+    
+    model = Sequential([ 
+	  resize_and_normalize,
+	  layers.Conv2D(16, 3, padding = 'same', activation = 'relu'),
+	  layers.MaxPooling2D(),
+	  layers.Flatten(),
+	  layers.Dense(128, activation = 'relu')
+	]) # classifier model architecture
+    model.build(input_shape = (None, 256, 256, 3)) # build model 
+    model.set_weights(weights) # put weights to model
+    
+    pred = model.predict(image) # prediction happens here
+    
+    return pred
+
---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from image_classifier import wolf_or_husky_classifier
+from tensorflow.keras.utils import load_img, img_to_array
+
+st.title('Wolf Vs. Husky Image Classification') # page title
+st.image('background_image.jpeg')
+
+image_path = st.file_uploader('Upload image') # widget to put media file
+
+if image_path is not None:
+    image_file = Image.open(image_path)
+    st.image(image_file, caption='Image uploaded successfully') # displays image
+
+    image_array = img_to_array(image_file) # converts pixels to numpy arrays
+    image_array = np.expand_dims(image_array, axis = 0)
+
+    pred = wolf_or_husky_classifier(image_array, 'model_weights.npy')
+    
+    if np.argmax(pred) == 0:
+        label = 'Given image is husky!'
+    else:
+        label = 'Given image is wolf!'
+
+    st.write(label)
+ 
+else:
+    st.write('Upload image again')
+
